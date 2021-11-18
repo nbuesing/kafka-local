73e96a208c1112877dde4dad83043e62d0e49d20
Neil Buesing <neil.buesing@rilldata.com>
2021-11-18 14:16:30 -0600

    script improvements, and moving remaining examples out of the projects and into the demo folder.

--- demo/eos/up.sh
+++ demo/eos/up.sh
@@ -4,7 +4,9 @@ cd $(dirname $0)/../..
 
 ./network.sh
 
-(cd kafka; docker compose up -d zookeeper broker-1 broker-2 broker-3 broker-4)
+# if broker-4 is commented out, do not try to start it -- script technically will work if any broker is commented out
+ACTIVE_BROKERS=$(sed -n -E -e "s/^  (broker-[0-9]):/\1 /p" ./kafka/docker-compose.yml | tr -d '\n')
+(cd kafka; docker compose up -d zookeeper ${ACTIVE_BROKERS})
 
 #(cd kafka-lb; docker compose up -d)
 
--- druid/unload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-spec=$1
-shift
-
-URL=http://localhost:38081
-
-curl -X POST $URL/druid/indexer/v1/supervisor/${spec}/reset
-curl -X POST $URL/druid/indexer/v1/supervisor/${spec}/shutdown
