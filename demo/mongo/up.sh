bc80e012022ba52140816dc5c007e90f7c0b452f
Neil Buesing <neil.buesing@rilldata.com>
2022-04-07 20:42:38 -0500

    adding SR dashboard; updating demos.

--- demo/mongo/up.sh
+++ demo/mongo/up.sh
@@ -10,7 +10,7 @@ cd $(dirname $0)/../..
 (cd mongo; docker compose up -d)
 (cd ksqlDB; docker compose up -d)
 
-#(cd monitoring; docker compose up -d )
+(cd monitoring; docker compose up -d )
 
 # takes the longest, but want to let others start while it is starting, so don't wait until evertyhing else has been started.
 (cd connect; docker compose up -d --wait)
